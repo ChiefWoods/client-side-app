b0c9cc166caac2190f90563ddbbc7c60245ff54f
ChiefWoods <chii.yuen@hotmail.com>
2024-09-30 02:44:39 +0800

    Fix buffer not defined

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,6 +1,7 @@
 import path from "path"
 import react from "@vitejs/plugin-react-swc"
 import { defineConfig } from "vite"
+import inject from '@rollup/plugin-inject'
 
 export default defineConfig({
   plugins: [react()],
@@ -9,4 +10,11 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    rollupOptions: {
+      plugins: [inject({
+        Buffer: ['buffer', 'Buffer']
+      })],
+    },
+  }
 })
