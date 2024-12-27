d0df8750f5aca66ba6622f84618707edabcac0f2
ChiefWoods <chii.yuen@hotmail.com>
2024-12-27 20:32:11 +0800

    Remove null program state

--- app/src/hooks/useAnchorProgram.tsx
+++ app/src/hooks/useAnchorProgram.tsx
@@ -1,28 +1,46 @@
 import { Mess } from "@/types/mess";
 import { AnchorProvider, Program } from "@coral-xyz/anchor";
-import { useAnchorWallet, useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { AnchorWallet, useConnection, useWallet } from "@solana/wallet-adapter-react";
 import { useCallback, useMemo, useState } from "react";
 import idl from "@/idl/mess.json";
 import { PublicKey, TransactionInstruction } from "@solana/web3.js";
 
 export function useAnchorProgram() {
-  const [program, setProgram] = useState<Program<Mess> | null>(null);
   const { connection } = useConnection();
-  const { publicKey } = useWallet();
-  const wallet = useAnchorWallet();
+  const wallet = useWallet();
+  const [program, setProgram] = useState<Program<Mess>>(
+    new Program(
+      idl as Mess,
+      new AnchorProvider(
+        connection,
+        wallet as AnchorWallet,
+        {
+          commitment: 'confirmed',
+        }
+      )
+    )
+  );
 
   useMemo(() => {
-    if (wallet) {
-      const provider = new AnchorProvider(connection, wallet, { commitment: 'confirmed' });
-      setProgram(new Program(idl as Mess, provider));
-    }
+    setProgram(
+      new Program(
+        idl as Mess,
+        new AnchorProvider(
+          connection,
+          wallet as AnchorWallet,
+          {
+            commitment: 'confirmed',
+          }
+        )
+      )
+    )
   }, [connection, wallet]);
 
   async function getInitIx(): Promise<TransactionInstruction> {
     return await program!.methods
       .init()
       .accounts({
-        payer: publicKey!,
+        payer: wallet.publicKey!,
       })
       .instruction();
   }
@@ -32,20 +50,18 @@ export function useAnchorProgram() {
       .send(text)
       .accounts({
         chat: chatPda!,
-        sender: publicKey!,
+        sender: wallet.publicKey!,
       })
       .instruction();
   }
 
   const getChatAcc = useCallback(async (chatPda: PublicKey) => {
-    if (program) {
-      return await program.account.chat.fetchNullable(chatPda);
-    }
+    return await program.account.chat.fetchNullable(chatPda);
   }, [program]);
 
-  return { 
+  return {
     getInitIx,
     getSendIx,
     getChatAcc,
-   };
+  };
 }
